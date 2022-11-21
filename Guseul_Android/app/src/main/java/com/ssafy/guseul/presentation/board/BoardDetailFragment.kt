264854ee29e5754ef4f352f88ed2ded813db00f4
serim53 <tpfla9953@naver.com>
2022-11-22 00:19:08 +0900

    feat: #4 커뮤니티 상세 화면, 글 작성 View 구현

---
+++ Guseul_Android/app/src/main/java/com/ssafy/guseul/presentation/board/BoardDetailFragment.kt
@@ -0,0 +1,20 @@
+package com.ssafy.guseul.presentation.board
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.guseul.R
+
+
+class BoardDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_board_detail, container, false)
+    }
+}
