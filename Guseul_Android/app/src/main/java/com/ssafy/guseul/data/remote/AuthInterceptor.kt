123a1d54aa35251ea10f7223a2589373d7cd8f66
serim53 <tpfla9953@naver.com>
2022-11-23 02:42:47 +0900

    setting : #7 Interceptor 구현과 Network Module 추가

---
+++ Guseul_Android/app/src/main/java/com/ssafy/guseul/data/remote/AuthInterceptor.kt
@@ -0,0 +1,14 @@
+package com.ssafy.guseul.data.remote
+
+import com.ssafy.guseul.data.local.datasource.SharedPreferences
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private val preferences: SharedPreferences) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+            .addHeader("Authorization", "Bearer " + preferences.accessToken).build()
+
+        return chain.proceed(request)
+    }
+}
