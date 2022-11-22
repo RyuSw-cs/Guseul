90ad6066b0faabbc36f350d3e91a5726e6f22b44
rsw1452@naver.com <rsw1452@gmail.com>
2022-11-23 02:23:35 +0900

    feat: #1 온보딩 페이지 구현

--- Guseul_Android/app/src/main/java/com/ssafy/guseul/data/remote/datasource/auth/model/AuthJWTResponse.kt
+++ Guseul_Android/app/src/main/java/com/ssafy/guseul/data/remote/datasource/auth/model/AuthJWTResponse.kt
@@ -1,7 +1,6 @@
 package com.ssafy.guseul.data.remote.datasource.auth.model
 
 import com.google.gson.annotations.SerializedName
-import com.ssafy.guseul.data.remote.datasource.base.BaseResponse
 import com.ssafy.guseul.data.remote.datasource.base.DataToDomainMapper
 import com.ssafy.guseul.domain.entity.auth.AuthUserAccessToken
 
@@ -10,12 +9,12 @@ data class AuthJWTResponse(
     var statusCode: Int? = null,
     @SerializedName("resMessage")
     var resMessage: String? = null,
-    @SerializedName("accessToken")
-    var accessToken: String? = null
+    @SerializedName("data")
+    var jwt: AuthAccessToken? = null
 ) : DataToDomainMapper<AuthUserAccessToken> {
     override fun toDomainModel(): AuthUserAccessToken {
         return AuthUserAccessToken(
-            accessToken = accessToken ?: ""
+            accessToken = jwt?.accessToken ?: ""
         )
     }
 }
--- Guseul_Android/app/src/main/java/com/ssafy/guseul/presentation/onboarding/adapter/OnBoardingBannerAdapter.kt
+++ Guseul_Android/app/src/main/java/com/ssafy/guseul/presentation/onboarding/adapter/OnBoardingBannerAdapter.kt
@@ -1,23 +1,21 @@
 package com.ssafy.guseul.presentation.onboarding.adapter
 
-import com.ssafy.guseul.presentation.uimodel.onboarding.OnBoardingBannerModel
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.ssafy.guseul.common.util.Constants.BUNDEL_KEY_ON_BOARDING_BANNER_DATA
+import com.ssafy.guseul.common.util.Constants.BUNDEL_KEY_ON_BOARDING_BANNER_POSITION
 import com.ssafy.guseul.presentation.onboarding.banner.OnBoardingBannerFragment
 
 class OnBoardingBannerAdapter(
     fragment: Fragment,
-    private val bannerList: List<OnBoardingBannerModel>
 ) : FragmentStateAdapter(fragment) {
 
-    override fun getItemCount() = bannerList.size
+    override fun getItemCount() = 3
 
     override fun createFragment(position: Int): Fragment {
         return OnBoardingBannerFragment().apply {
             arguments = Bundle().apply {
-                putParcelable(BUNDEL_KEY_ON_BOARDING_BANNER_DATA, bannerList[position])
+                putInt(BUNDEL_KEY_ON_BOARDING_BANNER_POSITION, position)
             }
         }
     }
