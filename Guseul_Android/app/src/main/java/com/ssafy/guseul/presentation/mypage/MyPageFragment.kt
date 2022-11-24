b42ef2bc42153bb08e5b1429c87609b9688ce5e3
serim53 <tpfla9953@naver.com>
2022-11-24 11:19:19 +0900

    feat : #12 마이페이지 내 스탬프 개수 등록

--- Guseul_Android/app/src/main/java/com/ssafy/guseul/presentation/mypage/MyPageFragment.kt
+++ Guseul_Android/app/src/main/java/com/ssafy/guseul/presentation/mypage/MyPageFragment.kt
@@ -1,33 +1,58 @@
 package com.ssafy.guseul.presentation.mypage
 
+import android.util.Log
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.ssafy.guseul.R
+import com.ssafy.guseul.common.util.setLoadingDialog
 import com.ssafy.guseul.databinding.FragmentMypageBinding
 import com.ssafy.guseul.presentation.base.BaseFragment
+import com.ssafy.guseul.presentation.base.ViewState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
 
-    val viewModel by viewModels<UserViewModel>()
+    private val userViewModel by viewModels<UserViewModel>()
+    private val historyViewModel by viewModels<HistoryViewModel>()
 
     override fun initView() {
+        //getCount()
         initData()
         initListener()
     }
 
+//    fun getCount() {
+//        historyViewModel.boardEntity.observe(viewLifecycleOwner) {
+//            when (it) {
+//                is ViewState.Success -> {
+//
+//                }
+//                else -> {
+//                    Log.d("getCount", "getCount: ")
+//                    // Do Nothing
+//                }
+//            }
+//        }
+//        historyViewModel.getUserHistory(userViewModel.userId)
+//    }
+
     fun initData() {
-        viewModel.userEntity.observe(viewLifecycleOwner) {
+        userViewModel.userEntity.observe(viewLifecycleOwner) {
             binding.tvProfileName.text = "${it.value?.nickname} 님"
             binding.tvProfileAddress.text = it.value?.address
-            //historyViewModel.getUserHistory(viewModel.userId)
         }
-        viewModel.getUserInfo()
+        userViewModel.count.observe(viewLifecycleOwner) {
+            binding.tvStamp.text = "구슬을 벌써 ${it}개 모았어요!"
+        }
+        userViewModel.getUserInfo()
+
     }
     fun initListener() {
         binding.apply {
-            tvHistory.setOnClickListener { navigate(MyPageFragmentDirections.actionMyPageFragmentToMyHistoryFragment(viewModel.userId)) }
-            tvModify.setOnClickListener { navigate(MyPageFragmentDirections.actionMyPageFragmentToModifyInfoFragment(viewModel.userId)) }
+            tvHistory.setOnClickListener { navigate(MyPageFragmentDirections.actionMyPageFragmentToMyHistoryFragment(userViewModel.userId)) }
+            tvModify.setOnClickListener { navigate(MyPageFragmentDirections.actionMyPageFragmentToModifyInfoFragment(userViewModel.userId)) }
         }
     }
 }
--- Guseul_Android/app/src/main/java/com/ssafy/guseul/presentation/mypage/UserViewModel.kt
+++ Guseul_Android/app/src/main/java/com/ssafy/guseul/presentation/mypage/UserViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ssafy.guseul.domain.entity.user.UserEntity
+import com.ssafy.guseul.domain.usecase.user.GetUserHistoryUseCase
 import com.ssafy.guseul.domain.usecase.user.GetUserUseCase
 import com.ssafy.guseul.presentation.base.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,11 +14,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class UserViewModel @Inject constructor(
-    private val getUserUseCase: GetUserUseCase
+    private val getUserUseCase: GetUserUseCase,
+    private val getUserHistoryUseCase: GetUserHistoryUseCase
 ) : ViewModel() {
 
     private val _userEntity = MutableLiveData<ViewState<UserEntity>>()
     val userEntity: LiveData<ViewState<UserEntity>> = _userEntity
+
+    private val _count = MutableLiveData<Int>()
+    val count: LiveData<Int> = _count
 
     var userId: Int = -1
 
@@ -27,6 +32,9 @@ class UserViewModel @Inject constructor(
             val response = getUserUseCase.getUserInfo()
             _userEntity.value = ViewState.Success(response)
             userId = response.userId ?: -1
+
+            _count.value = getUserHistoryUseCase.invoke(userId).size
+
         } catch (e: Exception) {
             _userEntity.value = ViewState.Error(e.message, null)
         }
